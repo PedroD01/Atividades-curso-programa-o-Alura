b4abba6f5e5aa80774acabcee3709d4d816322bb
Pedro Duarte <pdjtresr431@gmail.com>
2025-05-01 11:07:39 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,9 @@
+function sortear () {
+    let quant = parseIntdocument.getElementById('quantidade').value;
+    let de = parseIntdocument.getElementById('de').value;
+    let ate = parseIntdocument.getElementById('ate').value;
+
+    console.log(quant);
+    console.log(de);
+    console.log(ate);
+}
